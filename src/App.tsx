2bf3b2d7af532378a8f31665225c311a7d7476f9
임희지 <heeji@imhuijiui-MacBookAir.local>
2022-08-22 23:31:02 +0900

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as rxjs from 'rxjs';
 
 function App() {
+  const onChangeKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi</h1>
+      <input id={'keyword'} type={'text'} onChange={onChangeKeyword} />
+
+      <div id={'result'}></div>
     </div>
   );
 }
